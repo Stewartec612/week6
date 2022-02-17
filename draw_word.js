c94022b58df142e6d176846d8e7f4cd3512c0758
Stewart Christie <lt3778xi@go.minnstate.edu>
2022-02-17 14:43:06 -0600

    Picture for draw_word.html

---
+++ draw_word.js
@@ -0,0 +1,23 @@
+
+
+let canvas = document.querySelector('#canvas')
+let context = canvas.getContext( '2d')
+
+let input = document.querySelector('#image-text')
+
+let image = new Image()
+image.src = 'angry_ape.jfif'
+
+
+image.addEventListener('load', function (){
+    context.drawImage(image, 0,0)
+})
+
+input.addEventListener('input', function (){
+    let text = this.value
+    context.font = '30px Courier'
+    context.drawImage(image, 0, 0)
+    context.fillText(text, 450, 65)
+})
+
+
